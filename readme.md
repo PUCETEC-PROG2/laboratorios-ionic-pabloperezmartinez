9a03c7e48fca2b769526a74828ea10b322fe3304
Usuario invitado <Guest@iMacdePCEA10802.PUCE.EDU.EC>
2026-06-11 16:02:54 -0500

    Lab7. Lista de repositorios

--- readme.md
+++ readme.md
@@ -2,9 +2,9 @@
 
 ## Información del Estudiante
 
-- **Nombre:** [Tu nombre completo aquí]
-- **Carrera:** [Tu carrera]
-- **Fecha de inicio:** [Fecha]
+- **Nombre:** Pablo Pérez Martínez
+- **Carrera:** PUCETEC - Desarrollo de Software
+- **Fecha de inicio:** 11 de junio de 2026
 
 ---
 
---
+++ src/components/RepoItem.tsx
@@ -0,0 +1,32 @@
+import { IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonThumbnail } from '@ionic/react';
+import './RepoItem.css'
+import React from 'react'
+import { pencil, trash } from 'ionicons/icons';
+import { Repository } from '../interfaces/Repository';
+
+const RepoItem: React.FC<Repository> = (repository) => {
+    return (
+        <IonItemSliding>
+            <IonItem>
+                <IonThumbnail slot='start'>
+                    <img src={repository.avatarUrl} alt={repository.name} />
+                </IonThumbnail>
+                <IonLabel>
+                    <h3>{repository.name}</h3>
+                    <p>{repository.description}</p>
+                    <p><strong>Lenguaje: </strong>{repository.language}</p>
+                </IonLabel>
+            </IonItem>
+            <IonItemOptions>
+                <IonItemOption>
+                    <IonIcon icon={pencil} slot='icon-only' />
+                </IonItemOption>
+                <IonItemOption color="danger">
+                    <IonIcon icon={trash} slot='icon-only' />
+                </IonItemOption>
+            </IonItemOptions>
+        </IonItemSliding>
+    )
+}
+
+export default RepoItem
