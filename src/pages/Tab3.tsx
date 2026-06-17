2c904c0ad571663aadad127064dd5235e1d7d202
Usuario invitado <Guest@iMacdePCEA10802.PUCE.EDU.EC>
2026-06-17 15:47:55 -0500

    Lab 7. Template, Tab2, Tab3

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,31 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de Usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de Usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+
+        <div className="card-container">
+          <IonCard className="card">
+            <img src="https://avatars.githubusercontent.com/u/48026030?v=4" alt="Avatar" />
+            <IonCardHeader>
+              <IonCardTitle>Pablo Pérez Martínez</IonCardTitle>
+              <IonCardSubtitle>pabloperezmartinez</IonCardSubtitle>
+            </IonCardHeader>
+            <IonCardContent>
+               Desarrollador de software con experiencia en
+                  aplicaciones móviles y web. Apasionado por
+                  la tecnología, el aprendizaje continuo y la farra!!!.
+            </IonCardContent>
+          </IonCard>
+        </div>
+
       </IonContent>
     </IonPage>
   );
